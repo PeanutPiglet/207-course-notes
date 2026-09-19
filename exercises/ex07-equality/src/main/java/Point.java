c267c3d4223633dd9cde85a3007cc5f189640009
PeanutPiglet <jack.zzz.tang.zzz@gmail.com>
2026-09-18 23:27:08 -0400

    feat: implement equality

--- exercises/ex07-equality/src/main/java/Point.java
+++ exercises/ex07-equality/src/main/java/Point.java
@@ -46,8 +46,7 @@ public class Point {
    */
   @Override
   public String toString() {
-    // TODO
-    return "";
+    return String.format("(%d, %d)", this.getX(), this.getY());
   }
 
   /**
@@ -58,8 +57,10 @@ public class Point {
    */
   @Override
   public boolean equals(Object o) {
-    // TODO: check that o is a Point (use `instanceof`), cast it, and compare
-    //       the x and y fields.
+    if (o instanceof Point) {
+      Point casted = (Point) o;
+      return this.getX() == casted.getX() && this.getY() == casted.getY();
+    }
     return false;
   }
 
@@ -71,7 +72,6 @@ public class Point {
    */
   @Override
   public int hashCode() {
-    // TODO: Objects.hash(x, y) is an easy way to combine the fields.
-    return 0;
+    return Objects.hash(this.getX(), this.getY());
   }
 }
